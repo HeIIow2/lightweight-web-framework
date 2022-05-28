3b930108f17463c3561dfc7815729da22c63dec3
HeIIow2 <Lars.Noack@outlook.de>
2022-05-28 18:19:57 +0200

    finished all major features and documentation

--- readme.md
+++ readme.md
@@ -12,27 +12,39 @@ To import the framework, paste the following code in the head of your html file:
 
 ### header
 
-To create a header, use the `<header>` element. If you want a child of it to be on the right instead of the left,
+To create a header, use the `header` element. If you want a child of it to be on the right instead of the left,
 give it the `push` class.
 
 ### themes
 
-To create a button to switch between the currently available themes, simply create a `<button>` element with the class `theme-button`.
+To create a button to switch between the currently available themes, simply create a `button` element with the class `theme-button`.
 
 ### container
 
-For the contents you might want it responsive. You can create a `<div>` and give it the class `container`, to make every
+For the contents you might want it responsive. You can create a `div` and give it the class `container`, to make every
 child responsive. The container elements also keep a nice distance
 
 ### textbox
 
 The textbox is there to put your real content. The fields get highlighted and an outline will be made.
-Simply create a `<div>` with the class `textbox`. In a textbox you can use:
-- headers (`<h1>`, `<h2>`, `<h3>`)
-- blocktexts (`<p>`)
-- images (`<img>`)
+Simply create a `div` with the class `textbox`. In a textbox you can use:
+- headers (`h1`, `h2`, `h3`)
+- blocktexts (`p`)
+- images (`img`)
+- lists (`ol`, `ul`)
 
-you can give a blocktext the class ``secondary`` to make it... secondary.
+you can give a blocktext the class ``secondary`` to make it secondary and `elevated` to make it elevated.
+
+### images
+
+There are a couple of classes, you can add to your images in a textbox:
+- `negated`: the image gets negated when in dark mode.
+- `max`: the image spans over the whole textbox.
+- `half`: the image spans half the textbox.
+- `third`: the image spans over a third of the textbox.
+
+Due to responsive design the classes defining how far the image spans,
+may only apply on large screens.
 
 ### markdown support
 
---
+++ src/js/popup.js
@@ -0,0 +1,86 @@
+/*
+img src:
+
+https://pixabay.com/de/illustrations/unter-uns-symbol-mannschaftskamerad-6008615/
+Pixabay License
+Freie kommerzielle Nutzung
+Kein Bildnachweis nötig
+
+https://knowyourmeme.com/photos/2165975-pepe-the-frog
+*/
+
+const src_list = [
+    "assets/popup/among-us.png",
+    "assets/popup/pepe-the-frog.png"
+];
+let temp_list = src_list.slice();
+temp_list.splice(1, 1);
+
+const till_hide = [2000, 5000];
+const till_show = [10000, 20000];
+
+const queryString = window.location.search;
+
+// create dom element for popup
+let popup_elem = document.createElement("div");
+popup_elem.id = "popup-container";
+popup_elem.innerHTML = `
+    <img src="${random_src()}" class="hide" id="popup" alt="popup figure">
+  `;
+document.body.appendChild(popup_elem);
+
+/*
+<div id="popup-container">
+    <img src="assets/popup/among-us.png" className="hide" id="popup">
+</div>
+*/
+
+popup_elem = document.getElementById("popup");
+
+function show_popup() {
+    popup_elem.classList.remove('hide');
+}
+
+function random_src() {
+    var index = Math.floor(Math.random() * temp_list.length)
+    const new_src = temp_list[index];
+    if (temp_list.length <= 1) {
+        const latest_elem = temp_list[0];
+        temp_list = src_list.slice();
+        temp_list.splice(temp_list.indexOf(latest_elem), 1);
+    }else {
+        temp_list.splice(index, 1);
+    }
+    return new_src;
+}
+
+function hide_popup() {
+    popup_elem.classList.add('hide');
+
+    setTimeout(function () {
+        const new_src = random_src();
+        popup_elem.src = new_src;
+    }, 1000);
+}
+
+if (!(queryString.includes("hide"))) {
+    start_toggle();
+}
+
+function start_toggle() {
+    function random(min_max) {
+        return Math.floor(Math.random() * min_max[1]) + min_max[0];
+    }
+
+    setTimeout(function () {
+        show_popup();
+
+        setTimeout(function () {
+
+            hide_popup();
+            start_toggle();
+        }, random(till_hide))
+    }, random(till_show))
+}
+
+export {start_toggle};
